077951fe044327ba30111b2b6fc539f28345e35a
neerajain9 <neerajain@hotmail.com>
2021-10-26 13:31:49 -0400

    markers and circle

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -35,6 +35,8 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 // mapbox/satellite-v9
 // mapbox/satellite-streets-v11
 
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -44,3 +46,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
