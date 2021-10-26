db978493f2ae7b6f3dcddb31ab3b60db2585fcd5
neerajain9 <neerajain@hotmail.com>
2021-10-26 14:19:28 -0400

    mapping multiple points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -50,8 +50,61 @@ L.circleMarker([35.00811047057204, -120.56097154675285], {
  }).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// // let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// An array containing each city's location, state, and population.
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
+// Get data from cities.js
+let cityData = cities;
 
+// cities.forEach(i => {
+// cityData.forEach(i => {
+//   console.log(i);
+//   L.marker(i.location)
+//   .bindPopup("<h2>" + i.city + ", " + i.state + "</h2> <hr> <h3>Population " + i.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+//   });
+
+// Circle Marker
+cityData.forEach(i => {
+  console.log(i);
+  L.circleMarker(i.location, {
+    radius: i.population/100000,
+    color: "orange",
+    fillColor: "orange"
+  })
+  .bindPopup("<h2>" + i.city + ", " + i.state + "</h2> <hr> <h3>Population " + i.population.toLocaleString() + "</h3>")
+  .addTo(map);
+  });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
