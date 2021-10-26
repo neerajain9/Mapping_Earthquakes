a7e48cc1dc544e201b02b4df5aa679f2e56b10c0
neerajain9 <neerajain@hotmail.com>
2021-10-26 16:11:19 -0400

    mapping lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([41.62435877383866, -100.35821385461774], 4);
 
 // Alternate way to create the same 
 // (useful when we need to add multiple tile layers)
@@ -106,8 +106,40 @@ cityData.forEach(i => {
   .addTo(map);
   });
 
+  // Coordinates for each point to be used in the line.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+// ];
+
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
+
+let line = [
+  [37.6213, -122.3790],
+  [30.1976843582267, -97.66636363104423],
+  [43.67785724946157, -79.62479824424098],
+  [40.647547246734845, -73.79049148664551]
+];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  // color: "red"
+  color: "blue",
+  dashArray: '7, 7',
+  dashOffset: '7',
+  weight: 4,
+  opacity: 0.5
+}).addTo(map);
+
+// zoom the map to the polyline
+map.fitBounds(map.getBounds());
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
